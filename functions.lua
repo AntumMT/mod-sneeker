ea3ab210241dae9c84feca5cab550b7262f5b6ea
AntumDeluge <antumdeluge@gmail.com>
2017-05-29 01:16:02 -0700

    Change debug message in 'sneeker.log_debug'

--- functions.lua
+++ functions.lua
@@ -9,7 +9,7 @@ end
 -- Displays a message in log output only if 'sneeker.debug' is set to 'true'
 function sneeker.log_debug(message)
 	if sneeker.debug then
-		sneeker.log('[DEBUG] ' .. message)
+		sneeker.log('DEBUG: ' .. message)
 	end
 end
 
