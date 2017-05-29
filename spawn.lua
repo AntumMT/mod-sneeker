3a3de549bfd79c6d05ebdff5e1e0e003795d8538
AntumDeluge <antumdeluge@gmail.com>
2017-05-29 01:29:08 -0700

    Fix global name:
    - 'sneeker' not 'sneaker'

--- spawn.lua
+++ spawn.lua
@@ -26,7 +26,7 @@ minetest.register_abm({
 		local node_light = minetest.get_node_light(pos)
 
 		-- Debugging spawning
-		sneaker.log_debug('Node light level at ' .. tostring(pos.x) .. ',' .. tostring(pos.y) .. ': ' .. tostring(node_light))
+		sneeker.log_debug('Node light level at ' .. tostring(pos.x) .. ',' .. tostring(pos.y) .. ': ' .. tostring(node_light))
 
 		if not node_light then
 			return
