e35dd34462e7106ece87b5c0f3a99d0eca30e150
AntumDeluge <antumdeluge@gmail.com>
2017-08-08 01:38:12 -0700

    Add 'mobs_redo' compatibility (WIP)

--- spawn.lua
+++ spawn.lua
@@ -23,57 +23,101 @@ sneeker.log('Spawn chance: ' .. spawn_chance_percent)
 sneeker.log('Spawn interval: ' .. tostring(spawn_interval) .. ' (' .. tostring(spawn_interval/60) .. ' minute(s))')
 sneeker.log('Maximum light value for spawn: ' .. tostring(spawn_maxlight))
 
-core.register_abm({
-	nodenames = {'default:dirt_with_grass', 'default:stone'},
-	neighbors = {'air'},
-	interval = spawn_interval,
-	chance = spawn_chance,
-	action = function(pos, node, _, active_object_count_wider)
-		if active_object_count_wider > 5 then
-			return
+
+if core.global_exists('mobs') then
+	mobs:spawn({
+		name = sneeker.mob_name,
+		nodes = {'default:dirt_with_grass', 'default:stone'},
+		neighbors = {'air'},
+		min_light = -1,
+		max_light = spawn_maxlight,
+		interval = spawn_interval,
+		chance = spawn_chance,
+	})
+	
+	mobs:register_egg(sneeker.mob_name, 'Sneeker Spawn Egg', 'sneeker_spawnegg.png', 0, false)
+else
+	core.register_abm({
+		nodenames = {'default:dirt_with_grass', 'default:stone'},
+		neighbors = {'air'},
+		interval = spawn_interval,
+		chance = spawn_chance,
+		action = function(pos, node, _, active_object_count_wider)
+			if active_object_count_wider > 5 then
+				return
+			end
+			
+			-- Check light value of node
+			pos.y = pos.y+1
+			local node_light = core.get_node_light(pos)
+			
+			-- Debugging spawning
+			sneeker.log_debug('Node light level at ' .. sneeker.get_pos_string(pos) .. ': ' .. tostring(node_light))
+			
+			if not node_light or node_light > spawn_maxlight or node_light < -1 then
+				sneeker.log_debug('Node not dark enough for spawn')
+				return
+			end
+			
+			-- Spawn range
+			if pos.y > 31000 then
+				return
+			end
+			
+			-- Node must be touching air
+			if core.get_node(pos).name ~= 'air' then
+				return
+			end
+			pos.y = pos.y+1
+			if core.get_node(pos).name ~= 'air' then
+				return
+			end
+			
+			-- Get total count of sneekers in world
+			local count = 0
+			for I in pairs(core.luaentities) do
+			    if core.luaentities[I].name == sneeker.mob_name then
+			        count = count + 1
+			    end
+			end
+			
+			sneeker.log_debug('Current active spawns: ' .. tostring(count) .. '/' .. tostring(spawn_cap))
+			
+			if count >= spawn_cap then
+				sneeker.log_debug('Max spawns reached')
+				return
+			end
+			
+			sneeker.spawn(pos)
 		end
+	})
+	
+	
+	if core.get_modpath('spawneggs') and core.get_modpath('tnt') then
+		core.register_craftitem(sneeker.spawnegg_name, {
+			description = 'Sneeker Spawn Egg',
+			inventory_image = 'sneeker_spawnegg.png',
+			on_place = function(itemstack, placer, pointed_thing)
+				if pointed_thing.type == 'node' then
+					local pos = pointed_thing.above
+					pos.y = pos.y+1
+					core.add_entity(pos, sneeker.mob_name)
+					if not core.settings:get_bool('creative_mode') then
+						itemstack:take_item()
+					end
+					return itemstack
+				end
+			end
+		})
 		
-		-- Check light value of node
-		pos.y = pos.y+1
-		local node_light = core.get_node_light(pos)
+		core.register_craft({
+			output = sneeker.spawnegg_name,
+			type = 'shapeless',
+			recipe = {
+				'spawneggs:egg', 'tnt:tnt',
+			},
+		})
 		
-		-- Debugging spawning
-		sneeker.log_debug('Node light level at ' .. sneeker.get_pos_string(pos) .. ': ' .. tostring(node_light))
-		
-		if not node_light or node_light > spawn_maxlight or node_light < -1 then
-			sneeker.log_debug('Node not dark enough for spawn')
-			return
-		end
-		
-		-- Spawn range
-		if pos.y > 31000 then
-			return
-		end
-		
-		-- Node must be touching air
-		if core.get_node(pos).name ~= 'air' then
-			return
-		end
-		pos.y = pos.y+1
-		if core.get_node(pos).name ~= 'air' then
-			return
-		end
-		
-		-- Get total count of sneekers in world
-		local count = 0
-		for I in pairs(core.luaentities) do
-		    if core.luaentities[I].name == sneeker.mob_name then
-		        count = count + 1
-		    end
-		end
-		
-		sneeker.log_debug('Current active spawns: ' .. tostring(count) .. '/' .. tostring(spawn_cap))
-		
-		if count >= spawn_cap then
-			sneeker.log_debug('Max spawns reached')
-			return
-		end
-		
-		sneeker.spawn(pos)
+		core.register_alias('spawneggs:sneeker', sneeker.spawnegg_name)
 	end
-})
+end
